202a1ceeb4f07cb5a855bc335ee41e88bab6739f
kimkangseok0613 <ks0006131@gmail.com>
2025-01-14 17:06:47 +0900

    First Package Upload

---
+++ Runtime/SingleTon.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+// DoorManager
+namespace MyNamespace
+{
+    public class SingleTon<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T instance;
+        public static T Instance
+        {
+            get
+            {
+                instance = (T)FindAnyObjectByType(typeof(T));
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name, typeof(T));
+                    instance = obj.GetComponent<T>();
+                }
+                return instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+}
